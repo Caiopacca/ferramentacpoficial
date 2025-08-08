53dc5306d1b6b2be46bfe51f857f7b5a64bb69e4
Caio Pacca <paccarj@gmail.com>
2025-08-08 18:48:50 +0000

    o formulario esta sendo enviado mais nao esta liberando as ferramentas a

--- src/components/rd-station-form.tsx
+++ src/components/rd-station-form.tsx
@@ -10,20 +10,20 @@ export function RdStationForm() {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   useEffect(() => {
-    const handleConversion = (event: any) => {
-      // O RD Station dispara um evento 'submit_form_success' no window
-      // O 'detail' do evento contém o nome do formulário.
-      // Verificamos se é o formulário correto antes de liberar o botão.
-      if (event.detail.form_id === 'form-isca-ferramentas-gratuitas-3d422f4cbcae3b0a8fe6') {
+    const handleMessage = (event: MessageEvent) => {
+      if (
+        event.data.type === 'conversion' &&
+        event.data.payload.formId === '3d422f4cbcae3b0a8fe6'
+      ) {
         setIsFormSubmitted(true);
       }
     };
 
-    window.addEventListener('submit_form_success', handleConversion);
+    window.addEventListener('message', handleMessage);
 
     // Cleanup do event listener quando o componente é desmontado
     return () => {
-      window.removeEventListener('submit_form_success', handleConversion);
+      window.removeEventListener('message', handleMessage);
     };
   }, []);
 
@@ -36,8 +36,11 @@ export function RdStationForm() {
                 id="rdstation-forms-script"
                 src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"
                 onLoad={() => {
-                // @ts-ignore
-                new window.RDStationForms('form-isca-ferramentas-gratuitas-3d422f4cbcae3b0a8fe6', 'null').createForm();
+                  // @ts-ignore
+                  if (window.RDStationForms) {
+                    // @ts-ignore
+                    new window.RDStationForms('form-isca-ferramentas-gratuitas-3d422f4cbcae3b0a8fe6', 'null').createForm();
+                  }
                 }}
             />
         </>
