fee4ffda47a6abde549df174dcf6da0ed9f2aca9
Caio Pacca <paccarj@gmail.com>
2025-08-08 18:18:57 +0000

    Caixa de Ferramentas IA da CP Marketing mude para Caixa de Ferramentas d

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -97,7 +97,7 @@ export default function ToolsPage() {
           <Image src="https://firebasestorage.googleapis.com/v0/b/site-cp-marketing.firebasestorage.app/o/LOGO%20REDONDA%20EM%20SVG%20CP.svg?alt=media&token=973b78cf-9a80-4c4a-bac0-a66a058c392d" alt="Logo CP Marketing" width={60} height={60} className="mx-auto mb-4 rounded-md" />
             <div className="flex justify-center items-center gap-4">
               <h1 className="text-4xl md:text-5xl font-bold text-primary tracking-tight">
-                  Caixa de Ferramentas IA da CP Marketing
+                  Caixa de Ferramentas da CP Marketing
               </h1>
             </div>
           <p className="mt-4 text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto">
